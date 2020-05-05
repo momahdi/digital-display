ceda3c3bb6ce37b571b7a8a219120119e03b8163
momahdi <mohamed4mahdi@gmail.com>
2020-05-05 18:34:00 +0200

    first commit

---
+++ build/precache-manifest.6e06321ecd97926e70e8920087b251fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0a5ac3451002943584e8ef58c34940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26d766ccf8da48352f2e",
+    "url": "/static/css/main.c8f88344.chunk.css"
+  },
+  {
+    "revision": "ebca4ca280755f9185bc",
+    "url": "/static/js/2.31fadaf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31fadaf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d766ccf8da48352f2e",
+    "url": "/static/js/main.9ae8d8fd.chunk.js"
+  },
+  {
+    "revision": "6372d17a036b023cde19",
+    "url": "/static/js/runtime-main.e5f00107.js"
+  }
+]);
